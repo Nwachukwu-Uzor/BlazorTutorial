bf3f41af459a78af9d9b3b71e8aa90402ff2a9ca
Nwachukwu-Uzor <uzor1997@gmail.com>
2022-12-24 10:31:55 +0100

    adds reusable components to the blazor web app

---
+++ BlazorPractice.BlazerWasm/BaseComponents/DateTimeBaseComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorPractice.BlazerWasm.BaseComponents
+{
+    public class DateTimeBaseComponent : ComponentBase
+    {
+        public DateTime CurrentDateTime { get; set; }
+        public DateTimeBaseComponent()
+        {
+            CurrentDateTime = DateTime.Now;
+        }
+    }
+}
---
+++ BlazorPractice.BlazerWasm/Models/Team.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorPractice.BlazerWasm.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Region { get; set; }
+    }
+}
