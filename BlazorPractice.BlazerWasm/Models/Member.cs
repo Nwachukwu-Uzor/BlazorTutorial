b18bddd5112cea5f6431e0a7c3c79ef5e5046a0a
Nwachukwu-Uzor <uzor1997@gmail.com>
2022-12-26 18:56:47 +0100

    adds events callbacks, delegates and explicit state changes and refs to project

---
+++ BlazorPractice.BlazerWasm/Models/Member.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorPractice.BlazerWasm.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int TeamId { get; set; }
+    }
+}
